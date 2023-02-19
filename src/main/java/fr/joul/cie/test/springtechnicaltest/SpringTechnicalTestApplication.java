e9a90d34f0d4167c60093ee57a113b567be6030d
chakirardenne <ardenne.chakir@gmail.com>
2023-02-19 14:15:21 +0100

    Initial commit

---
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/SpringTechnicalTestApplication.java
@@ -0,0 +1,18 @@
+package fr.joul.cie.test.springtechnicaltest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechnicalTestApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTechnicalTestApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) throws Exception {
+		//Start here !
+	}
+}
