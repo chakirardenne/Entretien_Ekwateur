abf47ea6def96d011ad08f57b265117b740ec493
chakirardenne <ardenne.chakir@gmail.com>
2023-02-20 16:04:22 +0100

    build: Add mapstruct dependency for mapping dtos

---
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/application/converter/MapperTool.java
@@ -0,0 +1,24 @@
+package fr.joul.cie.test.springtechnicaltest.application.converter;
+
+import fr.joul.cie.test.springtechnicaltest.application.dto.CompatibleOfferDto;
+import fr.joul.cie.test.springtechnicaltest.application.dto.GetCompatibleOfferResponse;
+import fr.joul.cie.test.springtechnicaltest.domain.entities.Code;
+import fr.joul.cie.test.springtechnicaltest.domain.entities.Offer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ObjectFactory;
+
+@Mapper(componentModel = "spring")
+public interface MapperTool {
+
+    @ObjectFactory
+    default CompatibleOfferDto create(Offer offer) {
+        return new CompatibleOfferDto();
+    }
+    @Mapping(target = "type", source = "offerType")
+    @Mapping(target = "name", source = "offerName")
+    CompatibleOfferDto offerToCompatibleOfferDto(Offer offer);
+    @Mapping(target = "compatibleOfferDtos", ignore = true)
+    @Mapping(target = "promoCode", source = "code")
+    GetCompatibleOfferResponse codeToGetCompatibleOfferResponse(Code code);
+}
