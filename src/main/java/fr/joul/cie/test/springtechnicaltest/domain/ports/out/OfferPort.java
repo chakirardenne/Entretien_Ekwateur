e60143cdd82d3f4290d96f9264d14b3bb659ea07
chakirardenne <ardenne.chakir@gmail.com>
2023-02-20 19:11:51 +0100

    refactor: Refactor Ekwateur api port from two class to single class for better cohesion

--- src/main/java/fr/joul/cie/test/springtechnicaltest/domain/ports/out/OfferPort.java
+++
@@ -1,9 +0,0 @@
-package fr.joul.cie.test.springtechnicaltest.domain.ports.out;
-
-import fr.joul.cie.test.springtechnicaltest.domain.entities.Offer;
-
-import java.util.List;
-
-public interface OfferPort {
-    List<Offer> getAll();
-}
