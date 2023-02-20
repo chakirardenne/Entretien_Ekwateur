8859a24bff0e52c4b897e5b1e04435aa2237175b
chakirardenne <ardenne.chakir@gmail.com>
2023-02-20 16:00:33 +0100

    feat: Create dto for compatible offer use case

--- src/main/java/fr/joul/cie/test/springtechnicaltest/application/dto/CompatibleOfferDto.java
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/application/dto/CompatibleOfferDto.java
@@ -3,6 +3,7 @@ package fr.joul.cie.test.springtechnicaltest.application.dto;
 import lombok.Data;
 
 @Data
-public class GetCompatibleOfferRequest {
-    private String code;
+public class CompatibleOfferDto {
+    private String name;
+    private String type;
 }
--- src/main/java/fr/joul/cie/test/springtechnicaltest/application/dto/GetCompatibleOfferResponse.java
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/application/dto/GetCompatibleOfferResponse.java
@@ -1,4 +1,13 @@
 package fr.joul.cie.test.springtechnicaltest.application.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GetCompatibleOfferResponse {
+    private String promoCode;
+    private String endDate;
+    private Double discountValue;
+    private List<CompatibleOfferDto> compatibleOfferDtos;
 }
